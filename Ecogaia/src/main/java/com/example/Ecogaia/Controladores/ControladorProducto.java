026f32fe92ac0c3f70d7ce8d92b9e3230c45ab40
Yera <andresbarey165@gmail.com>
2023-03-27 16:07:52 -0500

    actcontrolador2

--- Ecogaia/src/main/java/com/example/Ecogaia/Controladores/ControladorProducto.java
+++ Ecogaia/src/main/java/com/example/Ecogaia/Controladores/ControladorProducto.java
@@ -1,8 +1,13 @@
 package com.example.Ecogaia.Controladores;
 
+import com.example.Ecogaia.Entidades.Producto;
 import com.example.Ecogaia.Servicios.ServicioProducto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
+@RestController
 public class ControladorProducto {
 
     ServicioProducto SP = new ServicioProducto();
@@ -13,8 +18,8 @@ public class ControladorProducto {
     } */
 
     @GetMapping("/BuscarProducto")
-    public BuscarProducto() {
-        return BuscarProducto;
+    public ArrayList<Producto> BuscarProducto() {
+        return SP.BuscarProducto;
     }
 
 
