e4229d5808929b2c11d19b078867bb04b557bedd
Yera <andresbarey165@gmail.com>
2023-04-27 10:40:48 -0500

    items

--- app/src/main/java/com/example/ecogaia/RecyclerHomeFragment.kt
+++ app/src/main/java/com/example/ecogaia/RecyclerHomeFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_nosotros.newInstance] factory method to
+ * Use the [RecyclerHomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_nosotros : Fragment() {
+class RecyclerHomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class fragment_nosotros : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nosotros, container, false)
+        return inflater.inflate(R.layout.fragment_recycler_home, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class fragment_nosotros : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_nosotros.
+         * @return A new instance of fragment RecyclerHomeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            fragment_nosotros().apply {
+            RecyclerHomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
