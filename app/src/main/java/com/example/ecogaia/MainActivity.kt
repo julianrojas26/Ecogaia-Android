7f91d8c36b76e03b3d47bae935308028aaaa45e6
angielpt2004 <angielpt.2004@gmail.com>
2023-06-26 19:54:06 -0500

    switch

--- app/src/main/java/com/example/ecogaia/MainActivity.kt
+++ app/src/main/java/com/example/ecogaia/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.menu.MenuView
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val host: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
                 ?: return
